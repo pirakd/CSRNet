3f1369a9512b9cff5cff5a8fc1181b31463d96be
pirakd <dpirak@gmail.com>
2023-03-22 14:39:34 +0200

    added train results logging

--- train.py
+++ train.py
@@ -10,11 +10,9 @@ import torch
 from torch.utils.data import DataLoader
 from os import path, makedirs
 from utils import get_root_path, redirect_output, names, get_time, log_args, load_checkpoint
+import json
 
-
-model_path = path.join('path', names.model_file)
-model_path = None
-
+model_path = None # change to load model from checkpoint
 
 root_path = get_root_path()
 output_folder = names.output_folder
@@ -28,26 +26,21 @@ log_args(output_file_path, config)
 model = CSRNet().to(device)
 loss_func = MSELoss(size_average=False)
 optimizer = Adam(model.parameters(), lr=config.learning_rate)
-
 if model_path:
     model, optimizer, start_epoch = load_checkpoint(model, optimizer, model_path)
+
 train_dataset_dir = config.train_set_dir
-test_dataset_dir = config.train_set_dir
-
-train_transforms = Compose([CropPatches(16, 5), RandomHorizontalFlip(), RandomVerticalFlip()])
+train_transforms = Compose([CropPatches(16, config.n_random_crops), RandomHorizontalFlip(), RandomVerticalFlip()])
 val_transforms = Compose([CropPatches(16, 0)])
-
 image_transforms = Compose([Normalize(mean=[0.5, 0.5, 0.5], std=[0.225, 0.225, 0.225])])
-
 train_dataset = DensityDataSet(train_dataset_dir, train_transforms, image_transforms, device=device)
 train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
-
 val_dataset = DensityDataSet(train_dataset_dir, val_transforms, image_transforms,  device=device)
 val_loader = DataLoader(val_dataset, batch_size=1, shuffle=True)
 
 trainer = Trainer(config.max_train_epochs, loss_func=loss_func, optimizer=optimizer,
                   eval_interval=config.eval_interval, device=device)
-
-
 train_stats, best_model = trainer.train(train_loader, val_loader, model, output_file_path=output_file_path)
 
+with open(path.join(output_file_path, 'train_results'), 'w') as f:
+    json.dump(train_stats, f, indent=4, separators=(',', ': '))
